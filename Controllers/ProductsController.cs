9a2d128169260350aa7a17a91f747a628fb24a4f
Boray Gulpinar <boray112@gmail.com>
2024-08-29 18:53:43 +0300

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private static readonly string[] Products =
+        {
+            "Iphone 14","IPhone 15","IPhone 16"
+        };
+
+        //localhost:5000/api/products => GET
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return Products;
+        }
+
+        //localhost:5000/api/products/1 => GET
+        [HttpGet("{id}")]
+        public string GetProducts(int id)
+        {
+            return Products[id];
+        }
+    }
+}
