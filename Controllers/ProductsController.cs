aafc44852291498e8a9949b129d37143ec7bd2d7
Boray Gulpinar <boray112@gmail.com>
2024-08-30 17:52:28 +0300

    commit mig

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Models;
 
 namespace ProductsAPI.Controllers
 {
@@ -8,23 +9,47 @@ namespace ProductsAPI.Controllers
     public class ProductsController : ControllerBase
     {
 
-        private static readonly string[] Products =
+        private static List<Product>? _products;
+
+        public ProductsController()
         {
-            "Iphone 14","IPhone 15","IPhone 16"
-        };
+            _products = new List<Product>
+            {
+                new() { ProductId = 1, ProductName = "IPhone 13", Price = 30000, IsActive = true },
+                new() { ProductId = 2, ProductName = "IPhone 14", Price = 40000, IsActive = true },
+                new() { ProductId = 3, ProductName = "IPhone 15", Price = 50000, IsActive = true },
+                new() { ProductId = 4, ProductName = "IPhone 16", Price = 60000, IsActive = true }
+            };
+        }
 
         //localhost:5000/api/products => GET
         [HttpGet]
-        public string[] GetProducts()
+        public IActionResult GetProducts()
         {
-            return Products;
+            if (_products == null)
+            {
+                return NotFound();
+            }
+            return Ok(_products);
         }
 
         //localhost:5000/api/products/1 => GET
         [HttpGet("{id}")]
-        public string GetProducts(int id)
+        public IActionResult GetProduct(int? id)
         {
-            return Products[id];
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var p = _products?.FirstOrDefault(i => i.ProductId == id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(p);
         }
     }
 }
---
+++ Models/ProductsContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProductsAPI.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 1, ProductName = "IPhone 13", Price = 30000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 2, ProductName = "IPhone 14", Price = 40000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 3, ProductName = "IPhone 15", Price = 50000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 4, ProductName = "IPhone 16", Price = 60000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 5, ProductName = "IPhone 17", Price = 70000, IsActive = false });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
